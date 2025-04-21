4744b56d4e8ef35220a8e97327afa8197c3a0ef2
jyotiReact <jyoti.itwaves@gmail.com>
2025-04-21 19:40:55 +0530

    Initial commit

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,46 @@
+"use client";
+import "../css/euclid-circular-a-font.css";
+import "../css/style.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+import { ModalProvider } from "../context/QuickViewModalContext";
+import { CartModalProvider } from "../context/CartSidebarModalContext";
+import { ReduxProvider } from "@/redux/provider";
+import QuickViewModal from "@/components/Common/QuickViewModal";
+import CartSidebarModal from "@/components/Common/CartSidebarModal";
+import { PreviewSliderProvider } from "../context/PreviewSliderContext";
+import PreviewSliderModal from "@/components/Common/PreviewSlider";
+
+import ScrollToTop from "@/components/Common/ScrollToTop";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <>
+          <ReduxProvider>
+            <CartModalProvider>
+              <ModalProvider>
+                <PreviewSliderProvider>
+                  <Header />
+                  {children}
+
+                  <QuickViewModal />
+                  <CartSidebarModal />
+                  <PreviewSliderModal />
+                </PreviewSliderProvider>
+              </ModalProvider>
+            </CartModalProvider>
+          </ReduxProvider>
+          <ScrollToTop />
+          <Footer />
+        </>
+      </body>
+    </html>
+  );
+}
